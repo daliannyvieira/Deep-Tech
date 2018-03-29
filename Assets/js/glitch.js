34dd5f83a10918082d25b4416b9b1a2ae30f82c8
daliannyvieira <vieiradalianny@gmail.com>
2018-03-29 18:49:13 -0300

    remove bot animation and fix belief width

--- Assets/js/glitch.js
+++ Assets/js/glitch.js
@@ -1,11 +1,6 @@
 {
-	let borboleta = document.querySelector(".borboleta");
-	let borboleto = document.querySelector(".borboleto");
-
 	setTimeout(() => document.body.classList.add('render'), 60);
 	imagesLoaded('.glitch__img', { background: true }, () => {
 		document.body.classList.add('imgloaded');
-		borboleta.classList.add('borboleta-toggle');
-		borboleto.classList.add('borboleto-toggle');
 	});
 }
